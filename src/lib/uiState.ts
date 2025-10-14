25b5b14c7502fefbab6ad41b15c52c1bd6ef42bc
maplebakin <madison.alway@gmail.com>
2025-10-14 11:48:59 -0400

    feat: centralize overlay state and refresh hero

---
+++ src/lib/uiState.ts
@@ -0,0 +1,9 @@
+import { signal } from '@preact/signals';
+
+export const menuOpen = signal(false);
+export const searchOpen = signal(false);
+
+export function closeAll() {
+  menuOpen.value = false;
+  searchOpen.value = false;
+}
